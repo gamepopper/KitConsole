9c0e5a9cb2819e7193447aec84741a961c3991bb
Tim Stoddard <gamepopper@users.noreply.github.com>
2024-04-19 22:18:40 +0100

    Implemented spiral demo to make it fancier.

--- Basic/Basic.cpp
+++ Basic/Basic.cpp
@@ -1,5 +1,5 @@
-#define SCREEN_WIDTH 80
-#define SCREEN_HEIGHT 60
+#define SCREEN_WIDTH 120
+#define SCREEN_HEIGHT 64
 #define FONTWIDTH 8
 #define FONTHEIGHT 8
 #define FPS 60
@@ -7,65 +7,29 @@
 //#define ENABLE_SOUND ///Optional define if a you want sound.
 #include <game.h>
 
+int f(int x, int y, float t)
+{
+	return static_cast<int>(sin(cos((x + 64) / t / 12)) + cos(sin((y + 64) / (t - 1) / 20)) + t) % 7;
+}
+
 void gameloop(float dt) ///Game Loops are done in here. dt is the delta time between frames (1 / FPS).
 {
-	clearScreen();
+	clearScreen();	
 
-	for (int i = 0; i < SCREEN_WIDTH / 2; i++)
+	//Spiral Demo by Jose Guerra: https://twitter.com/guerragames/status/1015430558699216896
+	float z = gameTime / 4.0f;
+	for (float a = 1.f; a > 0.05f; a -= 0.005f)
 	{
-		float s = ((float)3.0f * i / SCREEN_WIDTH) * sinf(gameTime + (10.0f * i / SCREEN_WIDTH));
-
-		int segmentHeight = 10;
-		int mainChar = 178;
-
-		if (i == 0)
+		for (int i = 1; i < 14; ++i)
 		{
-			segmentHeight = 6;
-			mainChar = 177;
+			float r = a * 60 + max(0, i * 3 * sinf((a + z) * (2 * PI)));
+			int x = static_cast<int>(r * cosf((a * 4 - z) * (2 * PI)));
+			int y = static_cast<int>(r * sinf((a * 4 - z) * (2 * PI)));
+			drawFillCircle((SCREEN_WIDTH / 2) + x, (SCREEN_HEIGHT / 2) + y, 3, 219, i);
 		}
-		if (i == 1)
-		{
-			segmentHeight = 9;
-		}
-
-		if (i >(SCREEN_WIDTH / 2) - 5)
-			segmentHeight = 9;
-		if (i > (SCREEN_WIDTH / 2) - 4)
-			segmentHeight = 8;
-		if (i > (SCREEN_WIDTH / 2) - 3)
-			segmentHeight = 6;
-		if (i > (SCREEN_WIDTH / 2) - 2)
-		{
-			segmentHeight = 4;
-			mainChar = 177;
-		}
-		if (i > (SCREEN_WIDTH / 2) - 1)
-		{
-			segmentHeight = 1;
-			mainChar = 176;
-		}
-
-		drawLine(SCREEN_WIDTH / 2 - SCREEN_WIDTH / 4 + i,
-			SCREEN_HEIGHT / 2 - segmentHeight + (s * 2),
-			SCREEN_WIDTH / 2 - SCREEN_WIDTH / 4 + i,
-			SCREEN_HEIGHT / 2 + segmentHeight + (s * 2),
-			mainChar, (i / 4) % 2 ? COLOUR::GREY : COLOUR::DARK_GREY);
-
-		drawChar(SCREEN_WIDTH / 2 - SCREEN_WIDTH / 4 + i,
-			SCREEN_HEIGHT / 2 - segmentHeight + (s * 2) + 1,
-			177, (i / 4) % 2 ? COLOUR::GREY : COLOUR::DARK_GREY);
-		drawChar(SCREEN_WIDTH / 2 - SCREEN_WIDTH / 4 + i,
-			SCREEN_HEIGHT / 2 + segmentHeight + (s * 2) - 1,
-			177, (i / 4) % 2 ? COLOUR::GREY : COLOUR::DARK_GREY);
-
-		drawChar(SCREEN_WIDTH / 2 - SCREEN_WIDTH / 4 + i,
-			SCREEN_HEIGHT / 2 - segmentHeight + (s * 2),
-			176, (i / 4) % 2 ? COLOUR::GREY : COLOUR::DARK_GREY);
-		drawChar(SCREEN_WIDTH / 2 - SCREEN_WIDTH / 4 + i,
-			SCREEN_HEIGHT / 2 + segmentHeight + (s * 2),
-			176, (i / 4) % 2 ? COLOUR::GREY : COLOUR::DARK_GREY);
 	}
 
+	//Console name in rainbow colours
 	const char* name =
 		" _  ___ _    ____                      _      " \
 		"| |/ (_) |_ / ___|___  _ __  ___  ___ | | ___ " \
